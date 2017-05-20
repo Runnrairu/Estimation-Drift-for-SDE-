d502ace58799e536aac047549085140451353b65
Kokoro <runnrairu@gmail.com>
2017-05-20 18:47:30 +0900

    Update code.py

--- code.py
+++ code.py
@@ -34,6 +34,7 @@ W=[0]*(n)
 Z=[0]*(n)
 b=[0]*(n)
 h= [[0] * (l+1) for i in range(n)]
+lam=[0]*n
 delta_t=T/l
 vol_t=delta_t**(1/2)
 for i in range(n):
@@ -41,17 +42,29 @@ for i in range(n):
 	k=i-2**m
 	for j in range(l+1):
 		if j>=k*T/2**m and j<(2*k+1)*T/2**(m+1):
-			h[i][j]=(j-k*T/2**m)*((2**m/T)**(1/2))
+			h[i][j]=(j-k*T/2.0**m)*((2**m/T)**(0.5))
 		elif (2*k+1)*T/2**(m+1)<=j and (k+1)*T/2**m:
-			h[i][j]=(T/2**m+2)**(1/2)-((2**m/T)**(1/2))*(j-(2*k+1)+T/2**(m+1))
+			h[i][j]=(T/2**m+2)**(1/2.0)-((2**m/T)**(0.5))*(j-(2*k+1)+T/2**(m+1))
 for i in range (l+1):
 	X[i+1]=X[i]+drift*delta_t+sigma*numpy.random.normal(0,vol_t)
 for i in range(n):
 	W[i]=numpy.random.normal(0,1)
 for i in range(n):
-	Z[i]=sigma*W[i]/lamda(i)
+	lam[i]=lamda(i)
 for i in range(n):
-	b[i]=uknaiseki(i)/lamda(i)
-fanction()
-
-
+	Z[i]=sigma*W[i]/lam[i]
+for i in range(n):
+	b[i]=uknaiseki(i)/lam[i]
+gx=0
+for i in range(n):
+	gx=gx+(b[i]+Z[i])**2
+malliavin_derive=[0]*(l+1)
+drift_estimator=[0]*(l+1)
+for j in range(l+1):
+	for i in range(n):
+		de_sum=de_sum+gx**((2-n)/2-1)*(b[i]+2*lam[i])*sigma**2*h[i][j]/lam[i]
+	malliavin_delive[j]=de_sum/gx**((2-n)/2)
+	drift_estimator[j]=malliavin_delive[j]+X[j]
+print X
+print malliavin_delive
+print estimator_drift
