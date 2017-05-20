3b8caea1539cfa02e937d402fbdd5039d36aca41
Kokoro <runnrairu@gmail.com>
2017-05-20 19:54:08 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 
 
+
 import math
 import numpy
 
@@ -22,9 +23,9 @@ def uknaiseki(i):
 	for j in range(l):
 		integralL2=integralL2+h[i][j+1]*X[j+1]*delta_t
 	return integralL2
-	
-m=10
-k=1
+
+m=6
+k=20
 n=2**m+k
 l=1000
 T=10.0
@@ -39,14 +40,16 @@ h= [[0] * (l+2) for i in range(n)]
 lam=[0]*n
 delta_t=T/l
 vol_t=delta_t**(1/2)
+
 for i in range(n):
 	m=han2(i)
 	k=i-2**m
 	for j in range(l+1):
-		if j>=k*T/2**m and j<(2*k+1)*T/2**(m+1):
-			h[i][j]=(j-k*T/2.0**m)*((2**m/T)**(0.5))
-		elif (2*k+1)*T/2**(m+1)<=j and (k+1)*T/2**m:
-			h[i][j]=(T/2**m+2)**(1/2.0)-((2**m/T)**(0.5))*(j-(2*k+1)+T/2**(m+1))
+		t=float(j/l)
+		if t>=k*T/2**m and t<(2*k+1)*T/2**(m+1):
+			h[i][j]=(t-k*T/2.0**m)*((2**m/T)**(0.5))
+		elif (2*k+1)*T/2**(m+1)<=t and (k+1)*T/2**m>t:
+			h[i][j]=(T/2**m+2)**(1/2.0)-((2**m/T)**(0.5))*(t-(2*k+1)+T/2**(m+1))
 for i in range (l+1):
 	X[i+1]=X[i]+drift*delta_t+sigma*numpy.random.normal(0,vol_t)
 for i in range(n):
@@ -57,6 +60,7 @@ for i in range(n):
 	Z[i]=sigma*W[i]/lam[i]
 for i in range(n):
 	b[i]=uknaiseki(i)/lam[i]
+
 gx=0
 
 for i in range(n):
@@ -66,9 +70,8 @@ drift_estimator=[0]*(l+1)
 for j in range(l+1):
 	de_sum=0
 	for i in range(n):
-		de_sum=de_sum+gx**((2-n)/2-1)*(b[i]+2*lam[i])*sigma**2*h[i][j]/lam[i]
+		de_sum=de_sum+gx**((2-n)/2.0-1)*(b[i]+2*lam[i])*sigma**2*h[i][j]/lam[i]
 	malliavin_delive[j]=de_sum/gx**((2-n)/2)
 	drift_estimator[j]=malliavin_delive[j]+X[j]
 print drift_estimator
-
 
