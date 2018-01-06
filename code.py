7f622d623f69ce06d3822c8d52710085c8ccac22
Kokoro <runnrairu@gmail.com>
2018-01-06 20:19:30 +0900

    Update code.py

--- code.py
+++ code.py
@@ -26,7 +26,7 @@ def X_h(k):#独自の工夫ポイント。カメロンマルティンの基底�
 
 
 T = 1.0  #終端時刻
-sigma = 0.5 #σの値
+sigma = 1.0 #σの値
 m = 1000 #時間の分割数
 t = 0 #初期時刻
 X = [0]*(m+1)  #確率微分方程式のサンプルパスを格納
@@ -35,24 +35,24 @@ loss_X = [0]*(m) #最尤推定の平均二乗誤差の記録用配列
 loss_XDF = [0]*(m) #シュタイン推定の平均二乗誤差の記録用配列
 delta_t = T/m  #Δt
 sigma_t = np.power(delta_t,0.5)#ブラウン運動実装のための標準偏差計算
-monte_count=1000 #モンテカルロ法を回す回数
+monte_count=100000 #モンテカルロ法を回す回数
 
 for l in range(monte_count):#モンテカルロのプログラム
     n=5 #近似の次元は5とした（カメロンマルティン空間は無限次元なので、どんなに増やしても完璧ではない）
-    X_h_array=[0]*(n+1) #X_h(k)を記録しておくことで計算を高速化する
+    X_h_array=[0]*(n) #X_h(k)を記録しておくことで計算を高速化する
     denominator=0 #シュタイン推定の補正項の分母。詳細は[1]を参照
-    delta_W = np.random.normal(0,sigma_t,m)#ブラウン運動実装用の乱数
+    delta_W = np.random.normal(0,sigma_t*sigma,m)#ブラウン運動実装用の乱数
+    t=0 #時刻のリセット
+    X[0]=0
     for i in range(m):#サンプルパスの実装
         t=t+delta_t
         X[i+1]=X[i]+drift(t)*delta_t+sigma*delta_W[i]
-        loss_X[i] += np.power(X[i]-drift(t),2)/monte_count #各自国で平均二乗誤差を計算しておく
+        loss_X[i] += np.power(X[i]-drift(t),2)/monte_count #各時刻で最尤推定の平均二乗誤差を計算しておく
     for k in range(n): 
         X_h_array[k]=X_h(k+1) #X_h(k)を記録していく
         denominator += np.power(X_h_array[k]/lamda(k+1),2)
-        
-
     for i in range(m):#推定量の構成と誤差の計測
-        t=(i+1)*T/m
+        t=i*T/m
         D_t_logF=0 #時刻tにおけるシュタイン推定の補正項
         for k in range(n):
             D_t_logF += X_h_array[k]*np.sin((k+1-0.5)*np.pi*t/T)
